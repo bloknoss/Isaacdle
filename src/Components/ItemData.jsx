fe26c6f2ec98427c22a9e37d1e49314d01e7c79e
bloknoss <windpourwow@gmail.com>
2024-03-03 23:39:18 +0100

    progress today

---
+++ src/Components/ItemData.jsx
@@ -0,0 +1,87 @@
+import "../styles/squarebox.css";
+
+function getColor(guess, real) {
+  guess = String(guess);
+  real = String(real);
+
+  return guess.toLowerCase() === real.toLowerCase() ? "green" : "red";
+}
+
+function Images({ guess }) {
+  return guess.pools[0] != "None" ? (
+    guess.pools_images.map((img) => (
+      <div
+        style={{
+          display: "flex",
+          textAlign: "center",
+          justifyContent: "center",
+        }}
+      >
+        <img style={{ width: "12px" }} src={img[0]}></img>
+        <img style={{ width: "12px" }} src={img[1]}></img>
+      </div>
+    ))
+  ) : (
+    <span>None</span>
+  );
+}
+
+function compareImages(item1, item2) {}
+
+export default function ItemData({ guess, real }) {
+  return (
+    <div className="square-container">
+      <div className="square">
+        <img src={guess.image} alt="" />
+      </div>
+      <div
+        style={{ backgroundColor: getColor(guess.type, real.type) }}
+        className="square"
+      >
+        <span>{guess.type}</span>
+      </div>
+      <div
+        style={{ backgroundColor: getColor(guess.tier, real.tier) }}
+        className="square"
+      >
+        <span>{guess.tier}</span>
+      </div>
+      <div
+        style={{
+          backgroundColor: getColor(
+            guess.transformations,
+            real.transformations
+          ),
+        }}
+        className="square"
+      >
+        <span>{guess.transformations}</span>
+      </div>
+      <div
+        style={{ backgroundColor: getColor(guess.pools, real.pools) }}
+        className="square"
+      >
+        <div style={{ alignItems: "center", textAlign: "center", top: "50%" }}>
+          <Images guess={guess}></Images>
+        </div>
+      </div>
+      <div
+        style={{
+          backgroundColor: getColor(guess.character, real.character),
+        }}
+        className="square"
+      >
+        <span>{guess.character}</span>
+      </div>
+
+      <div
+        className="square"
+        style={{
+          backgroundColor: getColor(guess.unlock_method, real.unlock_method),
+        }}
+      >
+        <span>{guess.unlock_method}</span>
+      </div>
+    </div>
+  );
+}
--- src/Components/QueryForm.jsx
+++ src/Components/QueryForm.jsx
@@ -1,56 +1,106 @@
 import { useEffect, useState } from "react";
 import logo from "../assets/logo.png";
+import ItemData from "./ItemData";
 
 export default function SearchForm() {
-  const [item, setItem] = useState([])
+  const [items, setItems] = useState([]);
   const [formValue, setFormValue] = useState({ itemName: "" });
+  const [itemComponents, setItemComponents] = useState([]);
+  const [suggestions, setSuggestions] = useState([]);
   const [data, setData] = useState(null);
-  useef
-  const fetchItems = async () => {
-    const response = await fetch(`/api/items/`);
-    setData(await response.json());
-  };
+  const [item, setItem] = useState([]);
+
+  useEffect(() => {
+    const rnd = parseInt(Math.random() * 710);
+    fetch(`/api/items/${rnd}`)
+      .then((res) => res.json())
+      .then((data) => setItem(data));
+
+    fetch(`/api/items/`)
+      .then((res) => res.json())
+      .then((data) => {
+        setItems(data);
+      });
+  }, []);
 
   const handleInput = (e) => {
     const { name, value } = e.target;
     setFormValue({ ...formValue, [name]: value });
+
+    const filteredSuggestions = [...items].filter((option) => option.name.toLowerCase().includes(value.toLowerCase()));
+
+    setSuggestions(filteredSuggestions);
   };
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    const fetchItem = async () => {
-      const response = await fetch(`/api/items/query/${formValue.itemName}`);
-      setData(await response.json());
-    };
-    fetchItem();
+    if (items.some((item) => item.name.toLowerCase().trim() == formValue.itemName.toLowerCase().trim())) {
+      const fetchItem = async () => {
+        const response = await fetch(`/api/items/query/${formValue.itemName}`);
+        const item = response.json();
+        setData(await item);
+        return item;
+      };
+
+      const guess = fetchItem();
+
+      await guess.then((guessItem) => {
+        addItemComponent(guessItem, item);
+        setItems(items.filter((x) => x.name !== guessItem.name));
+      });
+
+      setFormValue({ ...formValue, itemName: "" });
+      setSuggestions([]);
+    }
+  };
+
+  const handleSuggestionClick = (value) => {
+    setFormValue({ ...formValue, itemName: value.name });
+    setSuggestions([]);
+  };
+
+  const addItemComponent = (newGuess, newReal) => {
+    const newItemData = { guess: newGuess, real: newReal };
+    setItemComponents((prev) => [...prev, newItemData]);
   };
 
   return (
-    <div>
+    <>
       <img className="logo" src={logo} alt="" />
-      <div className="card shadow">
-        <form onSubmit={handleSubmit}>
-          <input
-            name="itemName"
-            value={formValue.itemName}
-            onChange={handleInput}
-            type="text"
-          />
-
-          <button type="submit">Lookup</button>
-        </form>
-        {data && (
-          <div className="item-info">
-            <h1>{data.name}</h1>
-            <span>{data.type}</span>
-            <img src={data.image} />
-            <span>{data.quote}</span>
-            <span>{data.id}</span>
-            <span>{data.tier}</span>
-            <span>{data.character}</span>
-          </div>
-        )}
+      <div className="container" style={{ overflow: "hidden", height: "80px" }}>
+        <div style={{ textWrap: "wrap", display: "flex", flexDirection: "column" }}>
+          <label htmlFor="itemName">Item Name</label>
+          <form onSubmit={handleSubmit}>
+            <input name="itemName" value={formValue.itemName} onChange={handleInput} autoComplete="off" type="text" />
+            {suggestions.length > 0 && (
+              <ul className="suggestions-list">
+                {suggestions.map((suggestion, index) => (
+                  <li key={index} onClick={() => handleSuggestionClick(suggestion)}>
+                    <img style={{ width: "32px" }} src={suggestion.image} alt="" />
+                    <span>{suggestion.name}</span>
+                  </li>
+                ))}
+              </ul>
+            )}
+            <input type="image" name="submit" src="src/assets/enter.png" className="enter-button" />
+          </form>
+          {itemComponents && <span style={{ marginTop: "5pxº" }}>Number of attempts: {itemComponents.length}</span>}
+        </div>
       </div>
-    </div>
+
+      <div className="container" style={{ height: "200px" }}>
+        <div className="squarebox-container">
+          {data && (
+            <>
+              {data.name === item.name ? <h1 style={{ color: "green" }}>Right Guess!!</h1> : <h1 style={{ color: "red" }}>Wrong Guess!!</h1>}
+
+              {itemComponents.map((data, index) => (
+                <ItemData key={index} guess={data.guess} real={data.real} />
+              ))}
+            </>
+          )}
+        </div>
+      </div>
+    </>
   );
 }
